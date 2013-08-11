fe0143d38dbb65fd08308d0049ce64051056d16c
Naiki Pohe <naiki.pohe@abletech.co.nz>
2013-08-12 08:51:55 +1200

    use css file for styling

--- banner.js
+++ banner.js
@@ -10,7 +10,7 @@ if (url != null) {
     var div = document.createElement("div");
     var imgURL = chrome.extension.getURL(environment+"_banner.png");
     div.innerHTML = "<img src="+imgURL+"\>";
-    div.style.cssText = "position: fixed;left: 0;top: 0;z-index: 1000;";
+    div.className = 'env_banner_wrapper'
     document.getElementsByTagName('body')[0].appendChild(div);
   }
 }
